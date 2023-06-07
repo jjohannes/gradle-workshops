fc1039607c804e69ad54ea8774dca3770ad702ea
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-06-07 17:16:30 +0200

    Testing / publishing

---
+++ business-logic/src/test/java/org/example/logic/MessageServiceTest.java
@@ -0,0 +1,15 @@
+package org.example.logic;
+
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("abc")
+public class MessageServiceTest {
+
+    @Test
+    void test1() {
+
+    }
+
+}
--- data-model/src/main/java/org/example/data/Message.java
+++ data-model/src/main/java/org/example/data/Message.java
@@ -1,5 +1,4 @@
 package org.example.data;
 
 public class Message {
-
 }
