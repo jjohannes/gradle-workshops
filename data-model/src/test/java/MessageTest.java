4eae5c3547863c6e9a09c7349c4e2063aacb788c
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-06-12 16:53:11 +0200

    Testing

--- data-model/src/test/java/MessageTest.java
+++ data-model/src/test/java/MessageTest.java
@@ -1,6 +1,12 @@
 import org.example.TestUtil;
+import org.junit.jupiter.api.Test;
 
 public class MessageTest {
 
-    // private TestUtil util = new TestUtil();
+    private TestUtil util = new TestUtil();
+
+    @Test
+    void test1() {
+        util.smth();
+    }
 }
--- data-model/src/testUtils/java/org/example/TestUtil.java
+++ data-model/src/testUtils/java/org/example/TestUtil.java
@@ -1,4 +1,7 @@
 package org.example;
 
 public class TestUtil {
+    public void smth() {
+
+    }
 }
