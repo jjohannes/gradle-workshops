66539a4d8d753fbbf6fb8addf9363c78a3108745
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-06-20 16:29:07 +0200

    Testing

---
+++ business-logic/src/test/java/org/example/MessageServiceSlowTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("slow")
+class MessageServiceSlowTest {
+
+    @Test
+    void test1() {
+
+    }
+}
---
+++ business-logic/src/test/java/org/example/MessageServiceTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+class MessageServiceTest {
+
+    @Test
+    void test1() {
+
+    }
+}
