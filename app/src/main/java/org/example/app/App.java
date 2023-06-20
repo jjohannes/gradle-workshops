d7f02341cdea24561eb43e5ef2623194bc4c386a
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-06-20 15:37:12 +0200

    Dependencies / catalog / dependency analysis

--- app/src/main/java/org/example/app/App.java
+++ app/src/main/java/org/example/app/App.java
@@ -1,8 +1,15 @@
 package org.example.app;
 
+import org.apache.commons.lang3.StringUtils;
+import org.example.service.MessageService;
+// import org.slf4j.LoggerFactory;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        StringUtils.capitalize("!!!");
+        // System.out.println("Hello!");
+        // LoggerFactory.getLogger(App.class).info("Hello!");
+        new MessageService();
     }
 }
--- business-logic/src/main/java/org/example/service/MessageService.java
+++ business-logic/src/main/java/org/example/service/MessageService.java
@@ -1,6 +1,14 @@
 package org.example.service;
 
+import org.example.data.Message;
+
 public class MessageService {
 
     private int x = 1;
+
+    private Message m = new Message();
+
+    private Message run() {
+        return new Message();
+    }
 }
