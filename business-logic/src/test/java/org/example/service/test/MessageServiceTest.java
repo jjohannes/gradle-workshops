41dc79f1a647486977fb60facb957e19dd5e8943
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-08-10 16:15:55 +0200

    Testing

---
+++ business-logic/src/test/java/org/example/service/test/MessageServiceTest.java
@@ -0,0 +1,12 @@
+package org.example.service.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MessageServiceTest {
+
+    @Test
+    public void testMessage() {
+        Assertions.assertEquals(2, 1 + 1);
+    }
+}
