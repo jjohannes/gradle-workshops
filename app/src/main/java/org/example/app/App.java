f773231a94d5fe7d38123016b3f8ede087cc3158
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-08-10 15:20:27 +0200

    Dependency Management

--- app/src/main/java/org/example/app/App.java
+++ app/src/main/java/org/example/app/App.java
@@ -1,7 +1,12 @@
 package org.example.app;
 
+import org.example.data.Message;
+import org.example.logic.MessageService;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello");
+        new MessageService().doSmth();
+        new Message();
     }
 }
--- business-logic/src/main/java/org/example/logic/MessageService.java
+++ business-logic/src/main/java/org/example/logic/MessageService.java
@@ -1,4 +1,14 @@
 package org.example.logic;
 
-public class MessageService {
+import org.apache.commons.lang3.StringUtils;
+import org.example.data.Message;
+import org.slf4j.LoggerFactory;
+
+public class MessageService extends Message {
+
+    public Message doSmth() {
+        StringUtils.capitalize("ss");
+        LoggerFactory.getLogger(MessageService.class).info("running service");
+        return new Message();
+    }
 }
