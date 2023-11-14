b1284fa13f9a9315a7c6b0fbd703566e37bd8fa3
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-11-14 13:02:24 +0100

    Publishing custom Jar / custom generation task

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -17,15 +17,53 @@ dependencies {
     meta("org.apache.commons:commons-text")
 }
 
+abstract class MyTask : DefaultTask() {
+
+    @get:InputDirectory
+    abstract val originalSource : DirectoryProperty
+
+    @get:OutputDirectory
+    abstract val generatedSource: DirectoryProperty
+
+    @TaskAction
+    fun gen() {
+        generatedSource.file("gen.txt").get().asFile.writeText("...")
+    }
+}
+
+val codeGeneration = tasks.register<MyTask>("codeGeneration") {
+    originalSource.set(layout.projectDirectory.dir("xyz"))
+    generatedSource.set(layout.buildDirectory.dir("internal-files"))
+}
+
+val internalJar = tasks.register<Jar>("internalJar") {
+    from(codeGeneration.flatMap { task -> task.generatedSource })
+    archiveClassifier = "internal"
+}
+
+configurations.runtimeElements {
+    outgoing.artifacts.clear()
+    outgoing.artifact(internalJar)
+}
+configurations.apiElements {
+    outgoing.artifacts.clear()
+    outgoing.artifact(internalJar)
+}
+
 publishing {
     publications.create<MavenPublication>("maven") {
+        // artifact(tasks.jar)
+        // artifact(internalJar)
+        from(components["java"])
         // versionMapping {
         //     allVariants {
         //         fromResolutionResult()
         //     }
         // }
-        from(components["java"])
     }
+}
+
+publishing {
     repositories {
         maven("../test-repo")
     }
