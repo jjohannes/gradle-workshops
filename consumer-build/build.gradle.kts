67dc5a4a27769b2eff6de15df60f80f187d4a53d
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-12-12 10:52:06 +0100

    Customizing publishing with custom 'outgoing variants'

--- consumer-build/build.gradle.kts
+++ consumer-build/build.gradle.kts
@@ -2,12 +2,31 @@ plugins {
     id("java-library")
 }
 
-repositories {
-    maven("../test-repo")
-    mavenCentral()
+val metaClasspath = configurations.create("metaClasspath") {
+    isCanBeConsumed = false
+    isCanBeResolved = true
+
+    // extendsFrom(configurations.implementation)
+
+    attributes {
+        attribute(Usage.USAGE_ATTRIBUTE, objects.named("meta"))
+    }
 }
 
 dependencies {
     api("org.example:core:1.0")
     implementation("com.fasterxml.jackson.core:jackson-databind")
+
+    metaClasspath("org.example:core:1.0")
+}
+
+tasks.register("printMeta") {
+    doLast {
+        println(metaClasspath.files)
+    }
+}
+
+repositories {
+    maven("../test-repo")
+    mavenCentral()
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,49 +6,48 @@ plugins {
 group = "org.example"
 version = "1.0"
 
-val meta = configurations.create("meta")
-
 dependencies {
     api(platform(project(":platform")))
 
     implementation("org.apache.commons:commons-text")
-
-    meta(platform("org.example:platform:1.0"))
-    meta("org.apache.commons:commons-text")
 }
 
-abstract class MyTask : DefaultTask() {
-
-    @get:InputDirectory
-    abstract val originalSource : DirectoryProperty
-
-    @get:OutputDirectory
-    abstract val generatedSource: DirectoryProperty
-
-    @TaskAction
-    fun gen() {
-        generatedSource.file("gen.txt").get().asFile.writeText("...")
-    }
-}
 
 val codeGeneration = tasks.register<MyTask>("codeGeneration") {
     originalSource.set(layout.projectDirectory.dir("xyz"))
     generatedSource.set(layout.buildDirectory.dir("internal-files"))
 }
-
 val internalJar = tasks.register<Jar>("internalJar") {
     from(codeGeneration.flatMap { task -> task.generatedSource })
     archiveClassifier = "internal"
 }
 
+// https://docs.gradle.org/current/userguide/publishing_customization.html#sec:adding-variants-to-existing-components
+// Adjust existing variant 'runtimeElements'
 configurations.runtimeElements {
-    outgoing.artifacts.clear()
+    outgoing.artifacts.clear() // remove 'tasks.jar' as artifact
     outgoing.artifact(internalJar)
 }
-configurations.apiElements {
-    outgoing.artifacts.clear()
+
+// Add a new variant 'metaElements'
+val metaElements = configurations.create("metaElements") { // configurations.consumable(...) since Gradle 8.4
+    isCanBeConsumed = true
+    isCanBeResolved = false
+
+    // extendsFrom(configurations.implementation.get())
     outgoing.artifact(internalJar)
+
+    attributes {
+        attribute(Usage.USAGE_ATTRIBUTE, objects.named("meta"))
+    }
 }
+
+dependencies {
+    metaElements("commons-codec:commons-codec:1.16.0")
+}
+
+// Make 'metaElements' a published 'outgoing variant'
+(components["java"] as AdhocComponentWithVariants).addVariantsFromConfiguration(metaElements) {}
 
 publishing {
     publications.create<MavenPublication>("maven") {
@@ -66,5 +65,20 @@ publishing {
 publishing {
     repositories {
         maven("../test-repo")
+    }
+}
+
+
+abstract class MyTask : DefaultTask() {
+
+    @get:InputDirectory
+    abstract val originalSource : DirectoryProperty
+
+    @get:OutputDirectory
+    abstract val generatedSource: DirectoryProperty
+
+    @TaskAction
+    fun gen() {
+        generatedSource.file("gen.txt").get().asFile.writeText("...")
     }
 }
