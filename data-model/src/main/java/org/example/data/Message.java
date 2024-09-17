ea1b540ae758333a2794cc179f769d075c51679f
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-09-17 15:56:16 +0200

    Dependencies

--- data-model/src/main/java/org/example/data/Message.java
+++ data-model/src/main/java/org/example/data/Message.java
@@ -1,4 +1,13 @@
 package org.example.data;
 
+import org.apache.commons.lang3.StringUtils;
+// import org.apache.commons.text.CaseUtils;
+
 public class Message {
+
+    public void convert(StringUtils util) {
+
+        // CaseUtils c;
+    }
+
 }
