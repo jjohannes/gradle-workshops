100011c9d321400528d5fa0e06dc9c4988d77cd8
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-09-17 11:20:17 +0200

    Project dependencies

--- app/src/main/java/org/example/app/App.java
+++ app/src/main/java/org/example/app/App.java
@@ -1,4 +1,11 @@
 package org.example.app;
 
+import org.example.logic.MessageService;
+
 public class App {
+
+    public static void main(String[] args) {
+        MessageService s;
+        System.out.println("Hello");
+    }
 }
