381085393190d12ceb19591627c8653dbc82ee71
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-12-08 15:24:23 +0100

    Testing setup

--- jamcatch/model/build.gradle.kts
+++ jamcatch/model/build.gradle.kts
@@ -1,3 +1,23 @@
 plugins {
     id("my-java-library")
 }
+
+
+
+// tasks.test {
+//     useJUnitPlatform()
+// }
+
+testing {
+    suites {
+        named<JvmTestSuite>("test") {
+            useJUnitJupiter()
+        }
+        register<JvmTestSuite>("xtest")
+    }
+}
+
+tasks.test {
+    maxParallelForks = 100
+    maxHeapSize = "2g"
+}
---
+++ jamcatch/model/src/test/java/org/example/javarca/model/ModelTest.java
@@ -0,0 +1,17 @@
+package org.example.javarca.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ModelTest {
+
+    @Test
+    void test1() {
+        Assertions.assertEquals("Hello World", "xxx");
+    }
+
+    @Test
+    void test2() {
+
+    }
+}
