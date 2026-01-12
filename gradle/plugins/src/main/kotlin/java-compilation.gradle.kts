2ea1bb58b196810de2d173a44e725fc705cadfd2
Jendrik Johannes <jendrik@onepiece.software>
2026-01-12 14:24:48 +0100

    Configure and register tasks

--- gradle/plugins/src/main/kotlin/java-compilation.gradle.kts
+++ gradle/plugins/src/main/kotlin/java-compilation.gradle.kts
@@ -6,6 +6,7 @@ java {
     toolchain.languageVersion = JavaLanguageVersion.of(21)
 }
 
-tasks.compileJava {
+tasks.compileJava { // == tasks.named("compileJava")
     options.encoding = "UTF-8"
+    options.compilerArgs.add("-Xlint:all")
 }
--- gradle/plugins/src/main/kotlin/my-java-application.gradle.kts
+++ gradle/plugins/src/main/kotlin/my-java-application.gradle.kts
@@ -6,3 +6,30 @@ plugins {
 application {
     mainClass = "org.example.jamcatch.game.GameApp"
 }
+
+// OLD SYNTAX:
+//
+// task bundle(Zip) {
+//
+// }
+// tasks.create("bundle", Zip::class) {
+//     from("build.gradle.kts")
+// }
+
+
+// https://docs.gradle.org/current/userguide/working_with_files.html
+// tasks.register("bundle", Sync::class) {
+tasks.register("bundle", Zip::class) {
+    group = "my"
+    description = "Assembles a Bundle"
+
+    // dependsOn(tasks.jar)
+    // from("build/libs/game.jar")
+    into("libs") {
+        from(tasks.jar)
+        from(configurations.runtimeClasspath)
+    }
+    from("build.gradle.kts")
+    // destinationDir = layout.buildDirectory.dir("bundle_dir").get().asFile
+    destinationDirectory = layout.buildDirectory.dir("bundle")
+}
