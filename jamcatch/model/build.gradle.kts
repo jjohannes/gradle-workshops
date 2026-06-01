c9c7e56be3323fe55f6073edfb37fef7d10ab5ab
Jendrik Johannes <jendrik@onepiece.software>
2026-06-01 11:17:27 +0200

    Fix dependencies

--- jamcatch/model/build.gradle.kts
+++ jamcatch/model/build.gradle.kts
@@ -1,11 +1,3 @@
 plugins {
     id("my-kotlin-library")
 }
-
-dependencies {
-    jvmMainApi(project(":engine"))
-    jvmMainImplementation("org.lwjgl:lwjgl-glfw:3.3.6")
-    jvmMainImplementation("org.lwjgl:lwjgl-opengl:3.3.6")
-    jvmMainImplementation("org.lwjgl:lwjgl-stb:3.3.6")
-    jvmMainImplementation("org.lwjgl:lwjgl:3.3.6")
-}
--- jamcatch/renderer/build.gradle.kts
+++ jamcatch/renderer/build.gradle.kts
@@ -1,3 +1,11 @@
 plugins {
     id("my-kotlin-library")
 }
+
+dependencies {
+    jvmMainApi(project(":engine"))
+    jvmMainImplementation("org.lwjgl:lwjgl-glfw:3.3.6")
+    jvmMainImplementation("org.lwjgl:lwjgl-opengl:3.3.6")
+    jvmMainImplementation("org.lwjgl:lwjgl-stb:3.3.6")
+    jvmMainImplementation("org.lwjgl:lwjgl:3.3.6")
+}
