7931b9edf1433af26185578da84bf65097e88daf
Jendrik Johannes <jendrik@onepiece.software>
2026-06-01 14:26:45 +0200

    Configure a task

--- gradle/plugins/src/main/kotlin/my-kotlin-app.gradle.kts
+++ gradle/plugins/src/main/kotlin/my-kotlin-app.gradle.kts
@@ -5,3 +5,27 @@ plugins {
 kotlin {
     jvm { mainRun { mainClass = "org.example.jamcatch.game.GameAppKt" } }
 }
+
+// tasks.named<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileKotlinJvm") {
+//     javaPackagePrefix = "xxxx"
+// }
+
+// tasks.create("bundle") <-- old API
+
+tasks.register<Zip>("bundle") {
+    from(layout.projectDirectory.file("build.gradle.kts"))
+
+    // from(layout.buildDirectory.file("libs/game-jvm.jar"))
+    // dependsOn(tasks.named("jvmJar"))
+
+    from(tasks.named<Jar>("jvmJar").flatMap { it.archiveFile });
+    group = "package"
+}
+
+tasks.named<Jar>("jvmJar") {
+    from("build.gradle.kts")
+
+    manifest {
+
+    }
+}
